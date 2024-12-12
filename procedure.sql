7d7aaba23f16838088c13a3ede35674fcd00117d
TrgPhan <23020443@vnu.edu.vn>
2024-12-12 23:54:56 +0700

    feat: update stored procedures and triggers for product management and sales tracking

--- procedure.sql
+++ procedure.sql
@@ -1,69 +1,58 @@
-DELIMITER $
-$
+DELIMITER $$
 
+-- QUẢN LÝ SẢN PHẨM
 
--- QUẢN LÝ SẢN PHẨM 
-
-
--- 1. LỌC SẢN PHẨM THEO MANUFACTURER
-DROP PROCEDURE IF EXISTS GetPhonesByManufacturer
-$$
-
-CREATE PROCEDURE GetPhonesByManufacturer(IN manufacturerName VARCHAR
-(50))
+-- LỌC SẢN PHẨM THEO MANUFACTURER
+DROP PROCEDURE IF EXISTS GetPhonesByManufacturer $$
+CREATE PROCEDURE GetPhonesByManufacturer(IN manufacturerName VARCHAR(50))
 BEGIN
-    SELECT pm.name AS PhoneName, pm.countView, pm.countSold, mf.name AS Manufacturer
+    SELECT 
+        pm.name AS PhoneName, 
+        pm.countView, 
+        pm.countSold, 
+        mf.name AS Manufacturer
     FROM phone_model pm
-        JOIN manufacturer mf ON pm.manufacturerID = mf.manufacturerID
+    JOIN manufacturer mf ON pm.manufacturerID = mf.manufacturerID
     WHERE mf.name = manufacturerName;
-END
-$$
+END $$
 
--- 2. LỌC SẢN PHẨM THEO GIÁ TIỀN
-DROP PROCEDURE IF EXISTS GetPhonesByPrice
-$$
+-- LỌC SẢN PHẨM THEO GIÁ TIỀN
+DROP PROCEDURE IF EXISTS GetPhonesByPrice $$
 CREATE PROCEDURE GetPhonesByPrice(IN minPrice INT, IN maxPrice INT)
 BEGIN
     SELECT
-        pm.name AS PhoneModel, pmo.name AS OptionName,
-        pmo.price AS Price, mf.name AS Manufacturer
+        pm.name AS PhoneModel, 
+        pmo.name AS OptionName,
+        pmo.price AS Price, 
+        mf.name AS Manufacturer
     FROM phone_model pm
-        JOIN phone_model_option pmo ON pm.phoneModelID = pmo.phoneModelID
-        JOIN manufacturer mf ON pm.manufacturerID = mf.manufacturerID
+    JOIN phone_model_option pmo ON pm.phoneModelID = pmo.phoneModelID
+    JOIN manufacturer mf ON pm.manufacturerID = mf.manufacturerID
     WHERE pmo.price BETWEEN minPrice AND maxPrice
     ORDER BY pmo.price ASC;
-END
-$$
+END $$
 
--- 3. LỌC SẢN PHẨM BÁN CHẠY TRONG THÁNG
-DROP PROCEDURE IF EXISTS GetBestSellingPhones
-$$
-CREATE PROCEDURE GetBestSellingPhones(IN targetMonth INT, IN targetYear INT)
+
+-- ĐỀ XUẤT CÁC SẢN PHẨM TƯƠNG TỰ DỰA TRÊN ID VỚI PHONE CONDITION KHÁC NHAU
+DROP PROCEDURE IF EXISTS GetSimilarPhones $$
+CREATE PROCEDURE GetSimilarPhones(IN phoneID INT)
 BEGIN
-    SELECT
-        pm.name AS PhoneModel, pmo.name AS OptionName,
-        SUM(od.quantity) AS TotalSold
-    FROM phone_model pm
-        JOIN phone_model_option pmo ON pm.phoneModelID = pmo.phoneModelID
-        JOIN order_detail od ON pmo.phoneModelOptionID = od.phoneModelOptionID
-        JOIN orders o ON od.orderID = o.orderID
-    WHERE MONTH(o.orderTime) = targetMonth AND YEAR(o.orderTime) = targetYear
-    GROUP BY pm.phoneModelID
-    ORDER BY TotalSold DESC;
-END
-$$
+	SELECT DISTINCT pmo.name as PhoneName,
+    p.customPrice, p.phoneCondition
+    FROM phone p
+    JOIN phone_model_option pmo ON pmo.phoneModelID = p.phoneModelID
+    WHERE p.phoneModelID IN 
+		(SELECT p1.phoneModelID FROM phone p1 
+		WHERE p1.phoneID = phoneID);
+END $$
 
 
 -- QUẢN LÝ CỬA HÀNG
 
 
--- 4. TÌM KIẾM ĐỊA CHỈ GẦN NGƯỜI DÙNG NHẤT 
-DROP PROCEDURE IF EXISTS GetNearbyStores
-$$
-
-CREATE PROCEDURE GetNearbyStores(IN userLongitude DECIMAL
-(10, 5), IN userLatitude DECIMAL
-(10, 5))
+-- TÌM KIẾM ĐỊA CHỈ GẦN NGƯỜI DÙNG NHẤT
+DROP PROCEDURE IF EXISTS GetNearbyStores $$
+CREATE PROCEDURE GetNearbyStores(IN userLongitude DECIMAL(10, 5), IN userLatitude DECIMAL(10, 5))
 BEGIN
     SELECT
         storeID,
@@ -73,36 +62,13 @@ BEGIN
         SQRT(POW(gps_longitude - userLongitude, 2) + POW(gps_latitude - userLatitude, 2)) AS Distance
     FROM store
     ORDER BY Distance ASC;
-END
-$$
-
--- 5. LẤY SẢN PHẨM ĐANG GIẢM GIÁ TRONG NGÀY
-DROP PROCEDURE IF EXISTS GetDiscountedProducts
-$$
-
-CREATE PROCEDURE GetDiscountedProducts(IN sDate DATE, IN eDate DATE)
-BEGIN
-    SELECT
-        pm.name AS ProductName,
-        pmo.price AS OriginalPrice,
-        pmd.discountPercent AS DiscountPercent,
-        (pmo.price - (pmo.price * pmd.discountPercent / 100)) AS DiscountedPrice
-    FROM promotion_detail_phone pmd
-        JOIN phone_model pm ON pm.phoneModelID = pmd.phoneModelID
-        JOIN phone_model_option pmo ON pmo.phoneModelOptionID = pmd.phoneModelOptionID
-        JOIN promotion p ON p.promotionID = pmd.promotionID
-    WHERE p.startDate <= sDate AND p.endDate >= eDate;
-END
-$$
+END $$
 
 
 -- QUẢN LÝ KINH DOANH
 
-
--- 6. LẤY DOANH THU HÀNG NGÀY
-DROP PROCEDURE IF EXISTS GetDailyRevenue
-$$
-
+-- LẤY DOANH THU HÀNG NGÀY
+DROP PROCEDURE IF EXISTS GetDailyRevenue $$
 CREATE PROCEDURE GetDailyRevenue(IN targetDate DATE)
 BEGIN
     SELECT
@@ -110,10 +76,34 @@ BEGIN
         COUNT(DISTINCT o.orderID) AS TotalOrders,
         SUM(od.finalPrice) AS TotalRevenue
     FROM orders o
-        JOIN order_detail od ON o.orderID = od.orderID
+    JOIN order_detail od ON o.orderID = od.orderID
     WHERE DATE(o.orderTime) = targetDate
     GROUP BY OrderDate;
-END
-$$
+END $$
+
+-- XUẤT HÓA ĐƠN
+DROP PROCEDURE IF EXISTS ExportInvoice $$
+CREATE PROCEDURE ExportInvoice(IN orderID INT)
+BEGIN
+    SELECT
+        o.orderID AS OrderID,
+        CASE 
+            WHEN od.serviceID = 0 THEN pmo.name
+            ELSE sd.description
+        END AS ItemName,
+        o.orderTime AS OrderTime,
+        od.originalPrice AS OriginalPrice,
+        od.finalPrice AS FinalPrice,
+        p.name AS DiscountName
+    FROM orders o
+    JOIN order_detail od ON o.orderID = od.orderID -- MAI CHECK LOGIC JOIN TÙM LUM
+    JOIN phone ph on ph.orderID = od.orderID
+    LEFT JOIN phone_model_option pmo ON ph.phoneModelOptionID = pmo.phoneModelOptionID
+    LEFT JOIN service_detail sd ON od.serviceID = sd.serviceID
+    LEFT JOIN promotion p ON od.promotionID = p.promotionID
+    WHERE o.orderID = orderID;
+END $$
 
 DELIMITER ;
+
+CALL ExportInvoice(1);
--- trigger.sql
+++ trigger.sql
@@ -1,75 +1,61 @@
-DELIMITER $
-$
+DELIMITER $$
 
--- CẬP NHẬT SỐ LƯỢNG ĐÃ BÁN CỦA PHONE_MODEL.
-DROP TRIGGER IF EXISTS after_order_insert
-$$
+-- CẬP NHẬT SỐ LƯỢNG ĐÃ BÁN CỦA PHONE_MODEL
+DROP TRIGGER IF EXISTS after_order_insert $$
 CREATE TRIGGER after_order_insert
-AFTER
-INSERT ON
-order_detail
-FOR
-EACH
-ROW
+AFTER INSERT ON order_detail
+FOR EACH ROW
 BEGIN
+    -- Cập nhật số lượng đã bán của phone model
     UPDATE phone_model
     SET countSold = countSold + 1
-    WHERE phoneModelID = (SELECT phoneModelID
-    FROM phone
-    WHERE phoneID = NEW.phoneID);
-END
-$$
+    WHERE phoneModelID = (
+        SELECT phoneModelID
+        FROM phone
+        WHERE phoneID = NEW.phoneID
+    );
+END $$
 
--- CẬP NHẬT GIÁ TIỀN ĐIỆN THOẠI NẾU NHƯ CÓ THÔNG TIN DISCOUNT MỚI.
-DROP TRIGGER IF EXISTS CalculateDiscountPrice
-$$
+-- CẬP NHẬT GIÁ TIỀN ĐIỆN THOẠI NẾU NHƯ CÓ THÔNG TIN DISCOUNT MỚI
+DROP TRIGGER IF EXISTS CalculateDiscountPrice $$
+
 CREATE TRIGGER CalculateDiscountPrice
-BEFORE
-INSERT ON
-order_detail
-FOR
-EACH
-ROW
+BEFORE INSERT ON order_detail
+FOR EACH ROW
 BEGIN
-    DECLARE discountPercent DECIMAL
-    (4,2);
+    DECLARE discountPercent DECIMAL(4,2);
     DECLARE discountFixed INT;
-DECLARE fixedNewPrice INT;
+    DECLARE fixedNewPrice INT;
 
--- Lấy thông tin giảm giá từ bảng promotion_detail_phone
-SELECT p.discountPercent, p.discountFixed, p.fixedNewPrice
-INTO discountPercent
-, discountFixed, fixedNewPrice
+    -- Lấy thông tin giảm giá từ bảng promotion_detail_phone
+    SELECT p.discountPercent, p.discountFixed, p.fixedNewPrice
+    INTO discountPercent, discountFixed, fixedNewPrice
     FROM promotion_detail_phone p
     WHERE p.promotionID = NEW.promotionID 
-      AND p.phoneModelID =
-(SELECT phoneModelID
-FROM phone
-WHERE phone.phoneID = NEW.phoneID)
-AND p.phoneModelOptionID =
-(SELECT phoneModelOptionID
-FROM phone
-WHERE phone.phoneID = NEW.phoneID)
-LIMIT 1;
+      AND p.phoneModelID = (
+        SELECT phoneModelID
+        FROM phone
+        WHERE phoneID = NEW.phoneID
+      )
+      AND p.phoneModelOptionID = (
+        SELECT phoneModelOptionID
+        FROM phone
+        WHERE phoneID = NEW.phoneID
+      )
+    LIMIT 1;
 
--- Tính toán giá trị finalPrice dựa trên thông tin giảm giá
-IF fixedNewPrice IS NOT NULL THEN
-SET NEW
-.finalPrice = fixedNewPrice;
+    -- Tính toán giá trị finalPrice dựa trên thông tin giảm giá
+    IF fixedNewPrice IS NOT NULL THEN
+        SET NEW.finalPrice = fixedNewPrice;
     ELSEIF discountPercent IS NOT NULL THEN
-SET NEW
-.finalPrice = NEW.originalPrice -
-(NEW.originalPrice * discountPercent / 100);
+        SET NEW.finalPrice = NEW.originalPrice - (NEW.originalPrice * discountPercent / 100);
     ELSEIF discountFixed IS NOT NULL THEN
-SET NEW
-.finalPrice = NEW.originalPrice - discountFixed;
+        SET NEW.finalPrice = NEW.originalPrice - discountFixed;
     ELSE
-SET NEW
-.finalPrice = NEW.originalPrice;
--- Không có giảm giá
-END
-IF;
+        SET NEW.finalPrice = NEW.originalPrice; -- Không có giảm giá
+    END IF;
 END $$
 
 -- TỰ ĐỘNG KÍCH HOẠT BẢO HÀNH CHO SẢN PHẨM MỚI MUA
+
 DELIMITER ;
